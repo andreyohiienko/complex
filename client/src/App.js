83798d3df4e0fc500d7ea8f960e787aaa1c03bcf
Andrey Ohiienko <andreyohiienko@gmail.com>
2020-10-05 18:11:19 +0300

    Add title

--- client/src/App.js
+++ client/src/App.js
@@ -11,9 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <h1 className="App-title">Fib calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other page</Link>
         </header>
